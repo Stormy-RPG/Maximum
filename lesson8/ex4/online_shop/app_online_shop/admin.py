b322e0d1e400ec1cd6b22d43efeacdeae4b6c8a7
Stormy_RPG <Stormy_RPG@mail.ru>
2023-08-27 21:10:06 +0400

    lesson8

---
+++ lesson8/ex4/online_shop/app_online_shop/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import OnlineShop
+
+# Register your models here.
+
+# создаём класс для отображения модели в панели администрирования
+class OnlineShopAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_time', 'update_time', 'auction']
+    list_filter = ['auction', 'created_time']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+
+    fieldsets = (
+        ('Общее', {
+            'fields': ('title', 'description')
+        }),
+        ('Финансы', {
+            'fields': ('price', 'auction'),
+            'classes': ['collapse']
+        })
+    )
+
+    @admin.action(description='Убрать возможность торга')
+    # request - запрос сайта, queryset - набор объектов, к которым применится созданный метод
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+    
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+
+# отображаем нашу модель в панели администрирования
+admin.site.register(OnlineShop, OnlineShopAdmin)
